7534d7f464753d86223dbdc09ef04fe86944732d
Toby Smith <toby@tismith.id.au>
2018-05-16 08:31:18 +1000

    feat: move to a lib/bin split for better tests

---
+++ src/bin/example-cli.rs
@@ -0,0 +1,10 @@
+//standard includes
+extern crate example_cli_rs;
+use example_cli_rs::*;
+
+fn main() -> Result<(), exitfailure::ExitFailure> {
+    let mut config = utils::cmdline::parse_cmdline();
+    config.module_path = Some(module_path!().into());
+    utils::logging::configure_logger(&config);
+    Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//standard includes
+//#[macro_use]
+pub extern crate failure; //this crate has macros, but this current program doesn't make use of them
+//#[macro_use]
+pub extern crate log;
+pub extern crate stderrlog;
+#[macro_use]
+pub extern crate clap;
+pub extern crate exitfailure;
+
+pub mod utils;
