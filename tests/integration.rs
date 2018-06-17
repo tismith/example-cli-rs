1ef21a798304871c9faceda17a5c98b945321c4c
Toby Smith <toby@tismith.id.au>
2018-06-17 10:24:28 +1000

    wip: testing kcov for integrationt test

--- tests/integration.rs
+++ tests/integration.rs
@@ -10,9 +10,13 @@ fn get_cwd() -> String {
 #[test]
 fn test_help() {
     //test that help works contains a USAGE string
-    let bin: &str = &format!("{}/target/debug/example-cli", get_cwd());
-    assert_cli::Assert::command(&[bin])
-        .with_args(&["-h"])
+    let cwd = get_cwd();
+    let kcov: &str = &format!("{}/target/kcov-master/build/src/kcov", &cwd);
+    let include: &str = &format!("--include-path={}", &cwd);
+    let bin: &str = &format!("{}/target/debug/example-cli", &cwd);
+    let args = ["--verify", include, bin, "-h"];
+    assert_cli::Assert::command(&[kcov])
+        .with_args(&args)
         .stdout()
         .contains("USAGE")
         .unwrap();
